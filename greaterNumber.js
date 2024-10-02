e07a178eec85d72b0ac9ca1b1406b16798427eff
Andrey Dimitrov <andrey89dimitrov@gmail.com>
2024-10-02 16:09:09 +0300

    Add files via upload

---
+++ greaterNumber.js
@@ -0,0 +1,11 @@
+function greaterNumber (arg1, arg2) {
+    let numbA = Number(arg1);
+    let numB = Number(arg2);
+
+    if (numbA > numB){
+ console.log (numbA)
+    } else {
+        console.log (numB)
+    }
+}
+greaterNumber (5, 6)
