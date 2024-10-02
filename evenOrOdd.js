e03996091ae94d907b54f1ff41d7e16fea1d812c
Andrey Dimitrov <andrey89dimitrov@gmail.com>
2024-10-02 16:19:43 +0300

    Add files via upload

---
+++ evenOrOdd.js
@@ -0,0 +1,10 @@
+function evenOrOdd (arg) {
+    let num = Number(arg);
+    if (num % 2 === 0) {
+        console.log ("even");
+    }else {
+        console.log ("odd")
+    }
+
+}
+evenOrOdd (1023)
