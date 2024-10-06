830016afdd49da7652a1eab1a8f052379b2d0e1b
Andrey Dimitrov <andrey89dimitrov@gmail.com>
2024-10-06 12:30:18 +0300

    Add files via upload

---
+++ lunchBreak.js
@@ -0,0 +1,13 @@
+function lunchBreak(seriesTitle, episodeDuration, breakDuration) {
+let lunchDuration = (1/8) * breakDuration;
+let lesiureDuration = (1/4) * breakDuration;
+let timeForMovie = breakDuration - lunchDuration - lesiureDuration;
+if (timeForMovie >= episodeDuration) {
+    let timeLeft = timeForMovie - episodeDuration;
+    console.log (`"You have enough time to watch ${seriesTitle} and left with ${Math.ceil(timeLeft)} minutes free time."`)
+} else {
+    let timeNeeded = episodeDuration - timeForMovie;
+    console.log(`You don't have enough time to watch ${seriesTitle}, you need ${Math.ceil(timeNeeded)} more minutes.`)
+}
+}
+lunchBreak ("Game of Thrones", 60, 96);
