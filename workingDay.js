83ee7e54b2099f1e421e6f01f58616fdccdae808
Andrey Dimitrov <andrey89dimitrov@gmail.com>
2024-10-12 14:22:59 +0300

    Add files via upload

---
+++ workingDay.js
@@ -0,0 +1,14 @@
+function weekendWorkingDay(day){
+    switch(day){
+        case "Monday": console.log("Working day"); break;
+        case "Tuesday": console.log("Working day"); break;
+        case "Wednesday": console.log("Working day"); break;
+        case "Thursady": console.log("Working day"); break;
+        case "Friday": console.log("Working day"); break;
+        case "Saturday": console.log("Weekend"); break;
+        case "Sunday": console.log("Weekend"); break;
+        default: console.log("Error")
+
+    }
+}
+weekendWorkingDay("Friday")
