b0c5c8885fcf51b449f054caa5c4f3a06f8fa099
Andrey Dimitrov <andrey89dimitrov@gmail.com>
2024-10-14 17:42:34 +0300

    Add files via upload

---
+++ newHome.js
@@ -0,0 +1,44 @@
+function newHome(flowerType, flowerCount, budget) {
+let totalPrice = 0;
+switch (flowerType) {
+    case "Roses":
+        totalPrice = flowerCount * 5;
+        if (flowerCount > 80) {
+            totalPrice *= 0.9;
+        }
+        break;
+        case "Dahlias":
+            totalPrice = flowerCount * 3.80;
+            if (flowerCount > 90) {
+                totalPrice *= 0.85;
+            }
+            break;
+            case "Tulips":
+                totalPrice = flowerCount * 2.80;
+                if (flowerCount > 80) {
+                    totalPrice *= 0.85;
+                }
+               break;
+               case "Narcissus":
+                totalPrice = flowerCount * 3;
+                if (flowerCount < 120) {
+                    totalPrice *= 1.15;
+                }
+                break;
+                case "Gladiolus":
+                    totalPrice = flowerCount * 2.50;
+                    if (flowerCount < 80) {
+                        totalPrice *= 1.20;
+                    }
+                    break;
+}
+if (budget >= totalPrice) {
+    let moneyLeft = budget - totalPrice;
+    console.log(`Hey, you have a great garden with ${flowerCount} ${flowerType} and ${moneyLeft} leva left.`);
+    }else {
+        let moneyNeeded = totalPrice - budget;
+        console.log(`Not enough money, you need ${moneyNeeded.toFixed(2)} leva more.`);
+        
+    }
+}
+newHome("Roses", 55, 250);
